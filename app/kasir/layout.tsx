8e1ced6a184e73724e19aabe274742c8a9479342
sutaaa0 <sutadev0@gmail.com>
2025-02-08 13:33:38 +0700

    update layout.tsx

--- app/kasir/layout.tsx
+++ app/kasir/layout.tsx
@@ -1,5 +1,3 @@
-import SidebarKasir from "@/components/SidebarKasir"
-import HeaderKasir from "@/components/HeaderKasir"
 import { Container } from "@/components/ui/container"
 
 export default function DashboardLayout({
@@ -9,9 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex h-screen bg-primary">
-      <SidebarKasir />
       <div className="flex-1 flex flex-col">
-        <HeaderKasir />
         <main className="flex-1 p-6">
           <Container className="h-full bg-white">{children}</Container>
         </main>
